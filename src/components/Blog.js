d016b5a95aa317df65e7063c69a0ea3a8c4f7e7c
akaro <roberto.castelli.teal@gmail.com>
2019-12-27 16:15:42 +0100

    missing links

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -6,7 +6,10 @@ function Blog() {
             <h1># BLOG</h1>
             <h3>Diary of an impostor</h3>
             <p>An evolving story of a 40 y.o. kid that wants to become an agile software craftman</p>
-            <button>TAKE ME THERE</button>
+            <div className="blog-buttons">
+                <button>TAKE ME THERE</button>
+                <button>LAST ARTICLE</button>
+            </div>
         </div>
     )
 }
